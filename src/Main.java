4c3d45bc5b316e58f8d623c333ebf587cf5d954a
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2022-02-01 21:26:24 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
-import javax.swing.*;
-import java.awt.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,28 +20,29 @@ public class Main {
             System.out.println("Enter website url: ");
             URL url = new URL(line.readLine());
 
-            //create file for the website
-            File file = new File("download.html");
+            //create folder for the website
+            String name = url.getHost().substring(0, url.getHost().indexOf("."));
+            Path path = Paths.get(name);
+            Files.createDirectory(path);
 
-            //download website codes and write them to download.html
+            //download website codes and write them to index.html in created directory
             URLConnection connection = url.openConnection();
             BufferedReader websiteReader = new BufferedReader(new InputStreamReader(url.openStream()));
-            BufferedWriter writer = new BufferedWriter(new FileWriter("download.html"));
-
+            BufferedWriter writer = new BufferedWriter(new FileWriter(name + "/" + "index.html"));
             String websiteLine;
             while((websiteLine = websiteReader.readLine()) != null){
                 writer.write(websiteLine);
             }
 
-            //open file
-            Desktop desktop = Desktop.getDesktop();
-            desktop.open(file);
+            //extracting links in the downloaded pages
+            File input = new File(name + "/" + "index.html");
+            Document doc = Jsoup.parse(input, "UTF-8", url + "/");
+            Elements links = doc.select("a[href]");
 
-            //close writer and reader
-            websiteReader.close();
-            writer.close();
 
-        }catch (Exception e){
+        } catch (FileAlreadyExistsException e){
+            System.out.println("Website already downloaded");
+        } catch (Exception e){
             e.printStackTrace();
         }
     }
