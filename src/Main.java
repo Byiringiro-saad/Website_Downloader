3c5275225cc7e27c850335705bfe21ee0a60f98f
Saad <61246755+Byiringiro-saad@users.noreply.github.com>
2022-02-01 22:10:29 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -37,11 +38,31 @@ public class Main {
             //extracting links in the downloaded pages
             File input = new File(name + "/" + "index.html");
             Document doc = Jsoup.parse(input, "UTF-8", url + "/");
-            Elements links = doc.select("a[href]");
+            Elements links = doc.select("a[href*='https://']");
+
+            //iterate every link and create its file
+            int iterator = 0;
+            for (Element element: links){
+                String link = element.attr("href");
+                URL newUrl = new URL(link);
+                URLConnection newConnection = newUrl.openConnection();
+                BufferedReader newWebsiteReader = new BufferedReader(new InputStreamReader(newUrl.openStream()));
+                BufferedWriter newWebsiteWriter = new BufferedWriter(new FileWriter(name+ "/" + "index" + iterator + ".html"));
+                String newWebsiteLine;
+
+                while((newWebsiteLine = newWebsiteReader.readLine()) != null){
+                    newWebsiteWriter.write(newWebsiteLine);
+                }
+                iterator += 1;
+            }
 
 
         } catch (FileAlreadyExistsException e){
             System.out.println("Website already downloaded");
+        } catch (MalformedURLException e){
+            System.out.println("Invalid url detected");
+        } catch (IOException e){
+            System.out.println("Server refused to respond");
         } catch (Exception e){
             e.printStackTrace();
         }
